af870a3442711ed59d341c3a8983b7f87cc25313
Evgeniy Baev <thedraik@gmail.com>
2017-05-11 11:38:10 +0200

    Run vacuumdb before backup.

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,5 +11,6 @@ test -f ~/.pgpass || { echo "${POSTGRES_HOSTNAME}:${PG_PORT}:${PG_DATABASE}:${PG
 test -d "${POSTGRES_FOLDER_BACKUP}" || mkdir "${POSTGRES_FOLDER_BACKUP}"
 { [ $# -gt 0 ] && exec "$@"; } && exit $?
 
+vacuumdb -h "${POSTGRES_HOSTNAME}" -U "${PG_USER}" -d "${PG_DATABASE}"
 find "${POSTGRES_FOLDER_BACKUP}/" -name *.backup -mtime +${BACKUP_KEEP_DAYS} -exec rm {} \;
 pg_dump -h "${POSTGRES_HOSTNAME}" -U "${PG_USER}" -Fc "${PG_DATABASE}" --no-owner > "${POSTGRES_FOLDER_BACKUP}/${FILE_NAME}"
