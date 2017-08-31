4979416c83cab3f380b9fe73881da610821a23bf
Evgeniy Baev <thedraik@gmail.com>
2017-08-31 14:54:13 +0200

    New backup logic. entrypoint.sh updated.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,21 +2,27 @@
 
 DATE=$(date +%Y%m%d%H%M)
 FILE_NAME="${PG_DATABASE}-database-${DATE}.backup"
-POSTGRES_FOLDER_BACKUP="/var/lib/postgresql/data/backups"
-POSTGRES_HOSTNAME="postgresql"
+PG_BACKUP_FOLDER="/var/lib/backups"
 
-test -f ~/.pgpass || { echo "${POSTGRES_HOSTNAME}:${PG_PORT}:${PG_DATABASE}:${PG_USER}:${PG_PASSWORD}" > ~/.pgpass && chmod 0600 ~/.pgpass; }
+set -e
 
-test -d "${POSTGRES_FOLDER_BACKUP}" || mkdir "${POSTGRES_FOLDER_BACKUP}"
+test -f ~/.pgpass || { echo "${PG_HOSTNAME}:${PG_PORT}:${PG_DATABASE}:${PG_USER}:${PG_PASSWORD}" > ~/.pgpass && chmod 0600 ~/.pgpass; }
+
+test -d "${PG_BACKUP_FOLDER}" || mkdir "${PG_BACKUP_FOLDER}"
 { [ $# -gt 0 ] && exec "$@"; } && exit $?
 
-echo "Operating on '${POSTGRES_HOSTNAME}:${PG_PORT}' and db: '${PG_DATABASE}' as user '${PG_USER}'"
+echo "Operating on '${PG_HOSTNAME}:${PG_PORT}' and db: '${PG_DATABASE}' as user '${PG_USER}'"
 
 echo "[$(date -Iseconds)] Running vacuumdb."
-vacuumdb -h "${POSTGRES_HOSTNAME}" -U "${PG_USER}" -d "${PG_DATABASE}"
+vacuumdb -h "${PG_HOSTNAME}" -U "${PG_USER}" -d "${PG_DATABASE}"
 
-echo "[$(date -Iseconds)] Removing backups older than ${BACKUP_KEEP_DAYS} days."
-find "${POSTGRES_FOLDER_BACKUP}/" -name *.backup -mtime +${BACKUP_KEEP_DAYS} -exec rm {} \;
+echo "REMOVE_OLD_BACKUPS = ${REMOVE_OLD_BACKUPS}"
+
+if [[ $REMOVE_OLD_BACKUPS == 1 ]]
+then
+	echo "REMOVE_OLD_BACKUPS = ${REMOVE_OLD_BACKUPS}"
+	/usr/bin/python3 /removeoldbackups.py -vv "${PG_BACKUP_FOLDER}"
+fi
 
 echo -e "[$(date -Iseconds)] Running pg_dump."
-pg_dump -h "${POSTGRES_HOSTNAME}" -U "${PG_USER}" -Fc "${PG_DATABASE}" --no-owner > "${POSTGRES_FOLDER_BACKUP}/${FILE_NAME}"
+pg_dump -h "${PG_HOSTNAME}" -U "${PG_USER}" -Fc "${PG_DATABASE}" --no-owner > "${PG_BACKUP_FOLDER}/${FILE_NAME}"
